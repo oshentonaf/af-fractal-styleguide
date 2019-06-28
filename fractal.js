49e1f3ef2454cdc59ed159c40e80cf7874a479dc
Olly Shenton <oliver.shenton@analogfolk.com>
2019-06-28 10:34:31 +0100

    set up 1

---
+++ fractal.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'AF Style Guide with Fractal');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+/*
+fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/* Set the static HTML build destination */
+fractal.web.set('builder.dest', __dirname + '/build');
+
+fractal.web.set('static.path', __dirname + '/public');
+
+fractal.web.set('server.sync', true);
+
+
+fractal.components.set('default.status', 'wip');
